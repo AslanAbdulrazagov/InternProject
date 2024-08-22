43b383ff04b13cc71aed1ac3b0e3429c7e7e1b8d
AslanAbdulrazagov <aslanabdulrazaqov496@gmail.com>
2024-08-22 12:09:17 +0400

    Docker was added....

--- src/Business/Services/Implementations/DepartmentService.cs
+++ src/Business/Services/Implementations/DepartmentService.cs
@@ -2,11 +2,6 @@
 using Business.Services.Interfaces;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Business.Services.Implementations
 {
